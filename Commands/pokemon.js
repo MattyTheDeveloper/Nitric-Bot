3e7097496c5a0c95331b93cdddd578de5a03afdc
Matt <37265551+MattyTheDeveloper@users.noreply.github.com>
2020-11-28 11:30:39 +0000

    Add files via upload

--- Commands/pokemon.js
+++ Commands/pokemon.js
@@ -4,6 +4,22 @@ module.exports = {
     name: 'pokemon',
     async execute(message,args,Discord, fetch) { // pokemon random
 
+        if (args.length == 10) {
+            const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon/arceus').then(response => response.json());
+            const embed = new Discord.MessageEmbed()
+            .setAuthor("ID: ["+pokemon.id+"] "+message.author.tag+'s Catch!')
+            .setColor(0x00AE86)
+            .setThumbnail(pokemon.sprites.front_default)
+           .addFields(
+                { name: 'A wild Pokemon appears!', value: 'arceus' },
+                { name: 'Level', value: '100' }
+          
+
+            )
+            .setTimestamp()
+        return message.channel.send({embed});
+        }
+
         let rando = Math.floor((Math.random() * 898) + 1);
         let rando1 = Math.floor((Math.random() * 100) + 1);
 
