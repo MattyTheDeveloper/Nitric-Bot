6b968d62539271143cba8847208abbccd2c5fd57
Matt <37265551+MattyTheDeveloper@users.noreply.github.com>
2020-11-28 11:13:02 +0000

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,29 @@
+const { execute } = require("./suggest")
+
+module.exports = {
+    name: 'pokemon',
+    async execute(message,args,Discord, fetch) { // pokemon random
+
+        let rando = Math.floor((Math.random() * 898) + 1);
+        let rando1 = Math.floor((Math.random() * 100) + 1);
+
+        const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon-form/'+rando).then(response => response.json());
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor("ID: ["+pokemon.id+"] "+message.author.tag+'s Catch!')
+            .setColor(0x00AE86)
+            .setThumbnail(pokemon.sprites.front_default)
+           .addFields(
+                { name: 'A wild Pokemon appears!', value: pokemon.name },
+                { name: 'Level', value: rando1 }
+          
+
+            )
+            .setTimestamp()
+        message.channel.send({embed});
+
+
+        
+
+    }
+}
