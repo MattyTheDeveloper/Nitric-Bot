914d7d8bfef5a3c44f10e28eb60caf6c0759c7a4
Matt <37265551+MattyTheDeveloper@users.noreply.github.com>
2020-11-28 14:26:21 +0000

    Add files via upload

--- reaction.js
+++ reaction.js
@@ -1,3 +1,5 @@
+const { execute } = require("./suggest");
+
 module.exports = {
     name: 'reaction',
     execute(message,args) {
@@ -5,12 +7,14 @@ module.exports = {
         if (message.member.roles.cache.has(role.id)) {
             const channel = message.guild.channels.cache.find(channel => channel.name === '📜info');
 
-            channel.send('> **Reaction Roles** @everyone\n\nReact to a message to get a specific role! These roles will ping you for different updates.  Simply react to gain a role, and un react to remove it.\n\n:loudspeaker:  | Announcements\n:newspaper:  | Changelog\n:tada:  | Events\n\n- Nitric Network').then(function (message) {
+            channel.send('> **Reaction Roles**\n\nReact to a message to get a specific role! These roles will ping you for different updates.  Simply react to gain a role, and un react to remove it.\n\n:loudspeaker:  | Announcements\n:newspaper:  | Changelog\n:tada:  | Events\n\n- Nitric Network').then(function (message) {
                 message.react('📢') 
                 message.react('📰')
                 message.react('🎉')
             });
             
+        } else {
+            console.log('no')
         }
     }
 }
